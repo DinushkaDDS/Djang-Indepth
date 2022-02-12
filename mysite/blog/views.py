42a9dddcd545dbd815d03486ef6109c841be78b3
Dilan Diushka <dds.dinushka@gmail.com>
2022-02-12 23:01:23 +0530

    added improvements

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_share(request, post_id):
@@ -41,8 +43,14 @@ class PostListView(ListView):
 
 
 # Not in use as Class based view is activated
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 1)
     page = request.GET.get('page')
     try:
@@ -56,7 +64,8 @@ def post_list(request):
 
     return render(request, 'blog/post/list.html', {
         'page': page,
-        'posts': posts
+        'posts': posts,
+        'tag': tag
     })
 
 
@@ -84,8 +93,14 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True) # flat=True helps to get an id list rather than id tuple list.
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
 
     return render(request, 'blog/post/detail.html',{'post': post,
                                                     'comments': comments,
                                                     'new_comment': new_comment,
-                                                    'comment_form': comment_form})
+                                                    'comment_form': comment_form,
+                                                    'similar_posts': similar_posts})
